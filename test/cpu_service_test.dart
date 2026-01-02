bb4d656733968c6758f3e4e5afd4bbdd94c98745
huynhtastic <huynhtastic@gmail.com>
2026-01-02 16:03:49 -0600

    test: added a performance test for cpuinfo
    Added 2 tests to test whether using a bytestream vs loading the entire
    cpuinfo file was better.

--- test/cpu_service_test.dart
+++ test/cpu_service_test.dart
@@ -1,58 +1,69 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gpu_usage_app/cpu_service.dart';
 
 const initialStat =
     '1 (test) S 0 0 0 0 0 0 0 0 0 0 100 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0';
 const initialUptime = '100.0 100.0';
 
 void main() {
-  late Directory tempDir;
-  late File statFile;
-  late File uptimeFile;
+  test('testing cpuinfo sync', () async {
+    final file = File('/proc/cpuinfo');
+    final sw = Stopwatch()..start();
 
-  setUp(() {
-    tempDir = Directory.systemTemp.createTempSync('cpu_service_test');
-    statFile = File('${tempDir.path}/stat');
-    uptimeFile = File('${tempDir.path}/uptime');
+    // --- Start Measurement ---
+    final lines = file.readAsLinesSync();
+    final model = lines.firstWhere((l) => l.contains('model name'));
+    // --- End Measurement ---
+
+    sw.stop();
+    print('Sync took: ${sw.elapsedMicroseconds}μs');
+    print('Memory used: ${ProcessInfo.currentRss / 1024} KB');
   });
 
-  tearDown(() {
-    if (tempDir.existsSync()) {
-      tempDir.deleteSync(recursive: true);
+  test('testing cpuinfo async', () async {
+    final file = File('/proc/cpuinfo');
+
+    // 1. Create the stream
+    final sw = Stopwatch()..start();
+    final a = file.openRead();
+    // .transform(utf8.decoder) // 2. Convert bytes to text
+    // .transform(const LineSplitter()); // 3. Split text into lines
+    sw.stop();
+    final openTime = sw.elapsedMicroseconds;
+    print('Opening took: $openTimeμs');
+
+    sw.reset();
+    sw.start();
+    final b = a.transform(utf8.decoder);
+    sw.stop();
+    final decodeTime = sw.elapsedMicroseconds;
+    print('Decoding took: $decodeTimeμs');
+
+    sw.reset();
+    sw.start();
+    final lines = b.transform(const LineSplitter()); // 3. Split text into lines
+    sw.stop();
+    final splitTime = sw.elapsedMicroseconds;
+    print('Splitting took: $splitTimeμs');
+
+    sw.reset();
+    sw.start();
+    try {
+      await for (var line in lines) {
+        if (line.startsWith('model name')) {
+          // How do we stop here to save resources?
+          break;
+        }
+      }
+    } catch (e) {
+      print('Error: $e');
     }
-  });
-
-  test('reading CPU usage 1 time shows 0', () async {
-    statFile.writeAsStringSync(initialStat);
-    uptimeFile.writeAsStringSync(initialUptime);
-
-    final cpuService =
-        CPUService(statPath: statFile.path, uptimePath: uptimeFile.path);
-    await cpuService.readCPUUsage();
-
-    expect(cpuService.usage, 0);
-  });
-
-  test('reading CPU usage 2 times shows 0.1', () async {
-    statFile.writeAsStringSync(initialStat);
-    uptimeFile.writeAsStringSync(initialUptime);
-
-    final cpuService =
-        CPUService(statPath: statFile.path, uptimePath: uptimeFile.path);
-
-    await cpuService.readCPUUsage();
-
-    // Second state: 110 ticks (+10), 101.0 uptime (+1.0)
-    // Formula: (deltaTicks / 100.0) / deltaTime * 100 => deltaTicks / deltaTime
-    // 10 / 1.0 = 10%
-    statFile.writeAsStringSync(
-        '1 (test) S 0 0 0 0 0 0 0 0 0 0 110 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0');
-    uptimeFile.writeAsStringSync('101.0 100.0');
-
-    await cpuService.readCPUUsage();
-
-    expect(cpuService.usage, 0.1);
+    sw.stop();
+    final searchTime = sw.elapsedMicroseconds;
+    print('Searching took: $searchTimeμs');
+    print('Total took: ${openTime + decodeTime + splitTime + searchTime}μs');
+    print('Memory used: ${ProcessInfo.currentRss / 1024} KB');
   });
 }
